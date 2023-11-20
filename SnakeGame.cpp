f8800a4667ed86cc194204bf5a88ca1e249ee70b
Selwyn John <guiruelarobin@gmail.com>
2023-11-20 15:47:11 +0800

    Added snake body function

--- SnakeGame.cpp
+++ SnakeGame.cpp
@@ -5,15 +5,87 @@
 using namespace std;
 
 bool gameOver;
+const int boardSize = 20;
+char board[boardSize][boardSize];
+int x, y, foodX, foodY;
+
+class Node{
+public:
+    char snakeBody;
+    Node* next;
+
+    Node(char snakeBody) {
+        this->snakeBody = snakeBody;
+        next = nullptr;
+    }
+};
+
+class Snake{
+    Node* head;
+    Node* tail;
+public:
+    Snake() {
+        head = nullptr;
+    }
+
+    void addBody(char body) {
+        Node* newNode = new Node(body);
+
+        if(head == nullptr) {
+            head = newNode;
+        } else {
+            Node* temp = head;
+            while(temp->next != nullptr) {
+                temp = temp->next;
+            }
+
+            temp->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void printSnake() {
+        Node* temp = head;
+
+        if(head == nullptr) {
+            cout << "List Empty" << endl;
+            return;
+        } 
+
+        while (temp != nullptr) {
+            cout << temp->snakeBody;
+            temp = temp->next;
+        }
+    }
+
+    void removeTail() {
+        if(head == nullptr) {
+            return;
+        }
+
+        if(head->next == nullptr) {
+            delete head;
+            head = nullptr;
+            tail = nullptr;
+            return;
+        }
+
+        Node* temp = head;
+        while(temp->next->next != nullptr) {
+            temp = temp->next;
+        }
+
+        delete temp->next;
+        temp->next = nullptr;
+        tail = temp;
+    }
+}; 
 
 void Setup() {
     gameOver = false;
 }
 
 void Draw() {
-    int boardSize = 20;
-    char board[boardSize][boardSize];
-
     system("CLS");
 
     for(int i = 0; i < boardSize; i++) {
@@ -45,7 +117,7 @@ int main() {
         Draw();
         Input();
         Logic();
-        Sleep(50);
+        Sleep(10);
     }
 
     return 0;
